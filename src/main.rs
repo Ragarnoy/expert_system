214bc11e74ad4bd94f665be4c702630c95691089
Tiago LERNOULD <tiagolernould@hotmail.com>
2019-10-08 21:45:10 +0200

    Added nom, still cant figure out parsing

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,59 @@
 /*   By: tlernoul <tlernoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/02 19:29:01 by tlernoul          #+#    #+#             */
-/*   Updated: 2019/10/05 17:59:09 by tlernoul         ###   ########.fr       */
+/*   Updated: 2019/10/08 21:43:28 by tlernoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #[macro_use]
 extern crate clap;
+extern crate nom;
 use clap::{Arg, App};
 use std::path::Path;
 use std::string;
 use std::fs;
+use std::io::{Error};
+
+struct Rule 
+{
+    Left: Vec<Operator>,
+    Right: Vec<Operator>,
+    Middle: Vec<Operator>,
+}
+
+enum Facts
+{
+    Res,
+    Fact,
+}
+
+enum Operator
+{
+    AND (Fact, Fact),
+    OR (Fact, Fact),
+    XOR (Fact, Fact),
+    THEN,
+}
+
+struct Fact
+{
+    name: String,
+    value: bool,
+}
+
+
+fn parse_and_return(input: String) -> Result<String, Error>
+{
+    for c in input.lines()
+    {
+        if c.chars().next().unwrap().is_alphanumeric()
+        {
+            println!("{}", c);
+        }
+    }
+    return Ok("truc".into())
+}
+
 
 fn main() 
 {
@@ -36,5 +79,6 @@ fn main()
         true => content = string::String::from(fs::read_to_string(file).unwrap()),
         false => panic!("Invalid input"),
     }
-    println!("{}", content);
+    parse_and_return(content);
+    //println!("{}", content);
 }
