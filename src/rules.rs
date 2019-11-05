62207bed781766adb06954265c2c01de69a83d35
Valentin SPORER <vsporer@e1r6p14.42.fr>
2019-11-05 15:29:58 +0100

    Add constructor for Rule (part of parser)

--- src/rules.rs
+++ src/rules.rs
@@ -7,3 +7,38 @@ pub struct Rule
     pub right: Factoken,
     pub middle: Operators,
 }
+
+impl Rule
+{
+	pub fn new(input: &str) -> Result<Self, String>
+	{
+		if input.matches("=>").count() == 1
+		{
+			let parts = input.split("=>");
+			let left = parts.next().unwrap_or("");
+			let right  = parts.next().unwrap_or("");
+			Ok(Rule
+			{
+				left: Factoken::new(left),
+				right: Factoken::new(right),
+				middle: Operators::Then
+			})
+		}
+		else if input.matches("<=>").count() == 1
+		{
+			let parts = input.split("<=>");
+			let left = parts.next().unwrap_or("");
+			let right  = parts.next().unwrap_or("");
+			Ok(Rule
+			{
+				left: Factoken::new(left),
+				right: Factoken::new(right),
+				middle: Operators::Then
+			})
+		}
+		else
+		{
+			Err(format!("a rule MUST contains exactly one `=>` OR `<=>` operator: {}", input))
+		}
+	}
+}
