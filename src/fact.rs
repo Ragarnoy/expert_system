e1988eaa6e478a40bd3e18c0ecc0ff4204a399d5
Valentin Sporer <v@sporer.fr>
2019-11-18 20:58:34 +0100

    Improve Fact::new() - more error case support

--- src/fact.rs
+++ src/fact.rs
@@ -40,36 +40,47 @@ impl Fact
 {
 	pub fn new(input: &str) -> Result<Self, String>
 	{
-		if input.len() == 2
+		let input_no_ws = input.replace(|c: char| c.is_ascii_whitespace(), "");
+		let chars = input_no_ws.char_indices();
+		let mut depth: isize = 0;
+		let mut not: Option<(usize, bool)> = None;
+		let mut fact: Option<char> = None;
+
+		for (i, c) in chars
 		{
-			let not = input.chars().nth(0).unwrap();
-			let fact = input.chars().nth(1).unwrap();
-			if not == '!' && fact.is_ascii_uppercase()
+			match c
 			{
-				Ok(Fact{
-					name: fact,
-					not: true,
-				})
+				'(' => depth += 1,
+				')' => depth -= 1,
+				'!' => match not {
+					Some((prev_i, prev_v)) if prev_i + 1 == i => not = Some((i, !prev_v)),
+					None if fact.is_some() => return Err(format!("`{}`: a fact cannot be declared with a NOT (`!`) after its name", input)),
+					None => not = Some((i, true)),
+					_ => return Err(format!("`{}`: NOT (`!`) is allowed only next to (excluding whitespaces) another NOT or a fact's name", input))
+				},
+				c if c.is_ascii_uppercase() => match not {
+					Some((index, _)) if index + 1 != i => return Err(format!("`{}`: NOT (`!`) is allowed only next to (excluding whitespaces) another NOT or a fact's name", input)),
+					_ if fact.is_some() => return Err(format!("`{}`: declare more than 1 fact here is forbidden", input)),
+					_ => fact = Some(c)
+				},
+				_ => return Err(format!("`{}` in `{}`: illegal token", c, input))
 			}
-			else
+		}
+		if depth != 0
+		{
+			return Err(format!("`{}`: mismatching parentheses", input));
+		}
+		if let Some(name) = fact
+		{
+			Ok(Self
 			{
-				Err(format!("`{}`: this cannot be a fact", input))
-			}
+				name,
+				not: not.unwrap_or((0, false)).1
+			})
 		}
 		else
 		{
-			let fact = input.chars().nth(0).unwrap();
-			if fact.is_ascii_uppercase()
-			{
-				Ok(Fact{
-					name: fact,
-					not: false,
-				})
-			}
-			else
-			{
-				Err(format!("`{}`: this cannot be a fact", input))
-			}
+			Err(format!("`{}`: we should have a fact around here", input))
 		}
 	}
 
