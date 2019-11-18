360c2025ace376b07e7b298d750498d44fa3e229
Valentin Sporer <v@sporer.fr>
2019-11-18 16:10:12 +0100

    Fix compile error + Add error propagation

--- src/rules.rs
+++ src/rules.rs
@@ -14,25 +14,25 @@ impl Rule
 	{
 		if input.matches("=>").count() == 1
 		{
-			let parts = input.split("=>");
+			let mut parts = input.split("=>");
 			let left = parts.next().unwrap_or("");
 			let right  = parts.next().unwrap_or("");
 			Ok(Rule
 			{
-				left: Factoken::new(left),
-				right: Factoken::new(right),
+				left: Factoken::new(left)?,
+				right: Factoken::new(right)?,
 				middle: Operators::Then
 			})
 		}
 		else if input.matches("<=>").count() == 1
 		{
-			let parts = input.split("<=>");
+			let mut parts = input.split("<=>");
 			let left = parts.next().unwrap_or("");
 			let right  = parts.next().unwrap_or("");
 			Ok(Rule
 			{
-				left: Factoken::new(left),
-				right: Factoken::new(right),
+				left: Factoken::new(left)?,
+				right: Factoken::new(right)?,
 				middle: Operators::Then
 			})
 		}
