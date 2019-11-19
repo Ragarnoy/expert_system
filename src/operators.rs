e373261f138ec698a3eb7f24927b2d82c76d6a55
Valentin SPORER <vsporer@e1r7p16.42.fr>
2019-11-19 17:26:42 +0100

    Reorder enum Operators for use of priority

--- src/operators.rs
+++ src/operators.rs
@@ -1,9 +1,12 @@
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
+// Please don't touch to the ordering.
+// The order of the 3 first operators is used to determine
+// the priority of an operator by casting the enum into a u32.
 pub enum Operators
 {
-    And,
-    Or,
     Xor,
+    Or,
+    And,
     Then,
     IfOnly,
 }
