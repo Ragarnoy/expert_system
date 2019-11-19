816a83b358aed272ab47f684e5bb97910e5ffd86
Valentin SPORER <vsporer@e1r7p16.42.fr>
2019-11-19 17:46:53 +0100

    Generate priority Vec used by Operation::new()

--- src/rules.rs
+++ src/rules.rs
@@ -1,4 +1,4 @@
-use crate::{operators::Operators, token::Factoken, fact::Fact};
+use crate::{operators::Operators, token::Factoken, fact::Fact, operation::Operation};
 use std::{collections::HashMap, iter::FromIterator};
 
 #[derive(Default, Debug, PartialEq, Eq, Hash, Clone)]
@@ -35,15 +35,21 @@ impl Rule
 	{
 		// TODO: We should search here for duplicated fact
 		// in both operation and conclusion side.
+
+        // TODO: We could (should ?) use match_indices() instead of matches here
+        // then pass use string slice instead of call split().
+        // This way is probably faster than the current one.
 		if input.matches("=>").count() == 1
 		{
 			let mut parts = input.split("=>");
 			let left = parts.next().unwrap_or("");
-			let right  = parts.next().unwrap_or("");
+			let right = parts.next().unwrap_or("");
+            let left_p = Operation::get_operators_sorted_by_priority(left);
+            let right_p = Operation::get_operators_sorted_by_priority(right);
 			Ok(Rule
 			{
-				left: Factoken::new(left)?,
-				right: Factoken::new(right)?,
+				left: Factoken::new(left, left_p)?,
+				right: Factoken::new(right, right_p)?,
 				middle: Operators::Then
 			})
 		}
@@ -51,12 +57,14 @@ impl Rule
 		{
 			let mut parts = input.split("<=>");
 			let left = parts.next().unwrap_or("");
-			let right  = parts.next().unwrap_or("");
+			let right = parts.next().unwrap_or("");
+            let left_p = Operation::get_operators_sorted_by_priority(left);
+            let right_p = Operation::get_operators_sorted_by_priority(right);
 			Ok(Rule
 			{
-				left: Factoken::new(left)?,
-				right: Factoken::new(right)?,
-				middle: Operators::Then
+				left: Factoken::new(left, left_p)?,
+				right: Factoken::new(right, right_p)?,
+				middle: Operators::IfOnly
 			})
 		}
 		else
