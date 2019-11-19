e36c99b40e17d41627359646dd952040b3474994
Valentin SPORER <vsporer@e1r7p16.42.fr>
2019-11-19 17:27:46 +0100

    Add some method on Operators and its constructor
    the constructor is already defined in another branch (algorithm ?)
    it's mandatory for the Operation constructor

--- src/operators.rs
+++ src/operators.rs
@@ -18,3 +18,48 @@ impl Default for Operators
 		Self::Then
 	}
 }
+
+impl Operators
+{
+	pub fn new(input: &str) -> Result<Self, String>
+	{
+		match input.len()
+		{
+			1 => match input.chars().nth(0).unwrap()
+			{
+				'+' => Ok(Operators::And),
+				'|' => Ok(Operators::Or),
+				'^' => Ok(Operators::Xor),
+				_ => Err(format!("`{}`: this cannot be an operators", input))
+			},
+			2 if input == "=>" => Ok(Operators::Then),
+			3 if input == "<=>" => Ok(Operators::IfOnly),
+			_ => Err(format!("`{}`: this cannot be an operators", input))
+		}
+    }
+
+    #[inline]
+    pub fn is_operator(c: char) -> bool
+    {
+        "+|^".contains(c)
+    }
+
+    // Here `depth` should represent how deep we are inside parentheses
+    pub fn get_priority(self, depth: isize) -> Option<usize>
+    {
+        let highest = Operators::get_highest_priority();
+        let priority = self as usize + 1;
+
+        if priority > highest || depth < 0
+        {
+            return None;
+        }
+        Some(highest * depth as usize + priority)
+    }
+
+    #[inline]
+    fn get_highest_priority() -> usize
+    {
+        Operators::And as usize + 1
+    }
+}
