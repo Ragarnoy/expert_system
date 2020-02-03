0f822f3b7d05345a2933c9df6ad4fc5b612948c9
Valentin SPORER <vsporer@e1r6p17.42.fr>
2020-02-03 11:20:37 +0100

    Remove duplicate `impl` block

--- src/operation.rs
+++ src/operation.rs
@@ -53,69 +53,7 @@ impl Operation
 	// then call the `new()` method on Factoken on each part of the operation:
 	// 	- the first part should be a substring from the beginning to operator excluded
 	//	- the second part should be a substring from the first char after the operator to the end
-	//
-	// /!\ WE STILL DON'T KNOW HOW TO HANDLE PARENTHESES PROPERLY HERE /!\
-	// pub fn new(input: &str) -> Result<Self, String>
-	// {
-	// 	let mut depth: i64 = 0;
 
-	// 	match 
-	// 	for c in input.chars()
-	// 	{
-	// 		match c
-	// 		{
-	// 			'(' => 
-	// 		}
-	// 	}
-	// }
-
-	pub fn resolve(&self, rules: &Vec<Rule>, known: &mut HashMap<Fact, Option<bool>>, seen: &mut HashMap<Rule, Vec<Fact>>) -> Option<bool>
-	{
-		self.operator.resolve(self.facts.0.resolve(rules, known, seen), self.facts.1.resolve(rules, known, seen))
-	}
-
-	pub fn resolve_as_conclusion(&self, rules: &Vec<Rule>, known: &mut HashMap<Fact, Option<bool>>, seen: &mut HashMap<Rule, Vec<Fact>>, result: bool) -> HashMap<Fact, Option<bool>>
-	{
-		// TODO: Handle OR and XOR conclusions
-		// Maybe a good idea to have a Vec<Fact> in the Operation struct
-		// to be able to quickly get all the facts an operation contains.
-		// Also, a FALSE conclusion even with only AND operators in it
-		// seems to be a bit more tricky than expected:
-		// from my understanding A + B => C + D means "if the expression `A AND B` is FALSE then the experession `C AND D` is FALSE"
-		// so C can be TRUE or FALSE as long as D is FALSE and the opposite is true as well.
-		// I really hope I'm wrong ! (Otherwise this method is wrong)
-		// Actually I'm maybe wrong (that would be great !) and I just need a break because this would lead to an undefined result for one of the fact
-		// but the subject of the project explicitly says that we can't have undefined value if we don't support OR and XOR conclusions.
-		// If a fact is FALSE and a rule which implies this fact returns FALSE is the fact set to FALSE or its is just not changed.
-		// (Read again if you don't see the difference)
-		match self.operator
-		{
-			Operators::And => HashMap::from_iter(self.get_facts().iter().map(|&f| {
-				(f, Some(if f.is_not() { !result } else { result }))
-			})),
-			_ =>
-			{
-				eprintln!("expert-system: OR and XOR in conclusion are not supported: {}", self);
-				std::process::exit(1);
-			}
-		}
-	}
-
-	pub fn get_facts(&self) -> Vec<Fact>
-	{
-		let mut facts = self.facts.0.get_facts();
-		facts.append(&mut self.facts.1.get_facts());
-		facts
-	}
-
-	pub fn contains_fact(&self, fact: &Fact) -> bool
-	{
-		self.facts.0.contains_fact(fact) || self.facts.1.contains_fact(fact)
-	}
-}
-
-impl Operation
-{
     // The value to pass for `priorities` should comes from the get_operators_sorted_by_priority() method.
     // Actually we obviously can get this priority list directly inside this method but I want to avoid doing the same thing
     // multiple times on the same (or part of the same) object. That also reduce the complexity of this method
@@ -183,4 +121,48 @@ impl Operation
         priorities.sort_by(|(_, p0, _), (_, p1, _)| p0.cmp(p1));
         Ok(priorities)
     }
+
+	pub fn resolve(&self, rules: &Vec<Rule>, known: &mut HashMap<Fact, Option<bool>>, seen: &mut HashMap<Rule, Vec<Fact>>) -> Option<bool>
+	{
+		self.operator.resolve(self.facts.0.resolve(rules, known, seen), self.facts.1.resolve(rules, known, seen))
+	}
+
+	pub fn resolve_as_conclusion(&self, rules: &Vec<Rule>, known: &mut HashMap<Fact, Option<bool>>, seen: &mut HashMap<Rule, Vec<Fact>>, result: bool) -> HashMap<Fact, Option<bool>>
+	{
+		// TODO: Handle OR and XOR conclusions
+		// Maybe a good idea to have a Vec<Fact> in the Operation struct
+		// to be able to quickly get all the facts an operation contains.
+		// Also, a FALSE conclusion even with only AND operators in it
+		// seems to be a bit more tricky than expected:
+		// from my understanding A + B => C + D means "if the expression `A AND B` is FALSE then the experession `C AND D` is FALSE"
+		// so C can be TRUE or FALSE as long as D is FALSE and the opposite is true as well.
+		// I really hope I'm wrong ! (Otherwise this method is wrong)
+		// Actually I'm maybe wrong (that would be great !) and I just need a break because this would lead to an undefined result for one of the fact
+		// but the subject of the project explicitly says that we can't have undefined value if we don't support OR and XOR conclusions.
+		// If a fact is FALSE and a rule which implies this fact returns FALSE is the fact set to FALSE or its is just not changed.
+		// (Read again if you don't see the difference)
+		match self.operator
+		{
+			Operators::And => HashMap::from_iter(self.get_facts().iter().map(|&f| {
+				(f, Some(if f.is_not() { !result } else { result }))
+			})),
+			_ =>
+			{
+				eprintln!("expert-system: OR and XOR in conclusion are not supported: {}", self);
+				std::process::exit(1);
+			}
+		}
+	}
+
+	pub fn get_facts(&self) -> Vec<Fact>
+	{
+		let mut facts = self.facts.0.get_facts();
+		facts.append(&mut self.facts.1.get_facts());
+		facts
+	}
+
+	pub fn contains_fact(&self, fact: &Fact) -> bool
+	{
+		self.facts.0.contains_fact(fact) || self.facts.1.contains_fact(fact)
+	}
 }
